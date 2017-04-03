65d3e411b2a2e9b3083cbc83ee6400b03fddfa73
amardeep <amardeep@ama>
2017-04-03 15:24:34 +0200

    First JSP

---
+++ first-web-application/src/main/java/com/amar/LoginServlet.java
@@ -0,0 +1,20 @@
+package com.amar;
+
+import java.io.PrintWriter;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(urlPatterns="/login.do")
+public class LoginServlet extends HttpServlet {
+
+	protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException{
+		
+		PrintWriter writer = response.getWriter();
+		writer.println("Dummy Amar");
+		request.getRequestDispatcher("/WEB-INF/views/FirstJspFile.jsp").forward(request, response);
+		
+	}
+	}
+	 
+
